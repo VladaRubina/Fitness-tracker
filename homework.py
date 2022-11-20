44b5ec2a5d0e4c0faba9a32cd66e1a90da81242c
Vlada Rubina <vlada.v.rubina@gmail.com>
2022-11-20 13:18:20 +0200

    домашка

--- homework.py
+++ homework.py
@@ -140,7 +140,7 @@ def read_package(workout_type: str, data: list) -> Training:
 
 def main(training: Training) -> None:
     """Главная функция."""
-    info: InfoMessage = training.show_training_info()
+    info = training.show_training_info()
     print(info.get_message())
 
 
