3a52eb3d1c5aa94fec2c2dc9cb0479a4b31fcf35
Vlada Rubina <vlada.v.rubina@gmail.com>
2022-11-20 12:52:26 +0200

    домашка

--- homework.py
+++ homework.py
@@ -18,11 +18,12 @@ class InfoMessage:
         self.calories = calories
 
     def get_message(self) -> str:
-        return ClassVar[str];('Тип тренировки: {training_type}; '
-                              'Длительность: {duration:.3f} ч.; '
-                              'Дистанция: {distance:.3f} км; '
-                              'Ср. скорость: {speed:.3f} км/ч; '
-                              'Потрачено ккал: {calories:.3f}.')
+        return str(f'Тип тренировки: {self.training_type}; '
+                   f'Длительность: {self.duration:.3f} ч.; '
+                   f'Дистанция: {self.distance:.3f} км; '
+                   f'Ср. скорость: {self.speed:.3f} км/ч; '
+                   f'Потрачено ккал: {self.calories:.3f}.'
+                   )
 
 
 class Training:
@@ -56,7 +57,7 @@ class Training:
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-        return InfoMessage(self.training_type,
+        return InfoMessage(type(self).__name__,
                            self.duration,
                            self.get_distance(),
                            self.get_mean_speed(),
